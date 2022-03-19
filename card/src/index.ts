be8d17525dcb3fd1a0435fb4f14e0d617beb2a42
Takesure Mamvota <tooyoungmamvota@gmail.com>
2022-03-20 04:54:36 +0700

    database connection

--- card/src/index.ts
+++ card/src/index.ts
@@ -5,25 +5,18 @@ import log from '../utils/logger'
 const app = express()
 app.use(express.json())
 
-app.get('/api/v1/card', (req, res)=>{
-    res.send('This is your card number')
-})
-
 const start = async () => {
 
-    // try {
-    //     await mongoose.connect('mongodb://auth-mongo-srv:27017/card');
-    //     log.info('Db Connected Successfully');
-    // } catch (err) {
-    //     log.error('Db not Connected');
-
-    // }
+    try {
+        await mongoose.connect('mongodb://mongo-srv:27017/card');
+        log.info('Db Connected Successfully');
+    } catch (err: any) {
+        log.error(err);
+    }
 
     app.listen(3000, () => {
-        log.info('Server listening on port 3000!!!')
+        log.info('Listening on port 3000!!!')
     })
 }
-
-
 
 start();
